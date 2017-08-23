786d0a878a5b66ed3b35541b15f1b3add32de643
David <david0718@qq.com>
2017-08-23 10:33:11 +0800

    update

--- SuperRocket.Console/Job/TestJob.cs
+++ SuperRocket.Console/Job/TestJob.cs
@@ -14,7 +14,7 @@ namespace SuperRocket.Orchard.Job
         public override void Execute(int number)
         {
             Thread.Sleep(5000);
-            Console.WriteLine("{0} Test job completed with {1} counts successfully!", DateTime.Now.ToString(), number);
+            System.Console.WriteLine("{0} Test job completed with {1} counts successfully!", DateTime.Now.ToString(), number);
         }
     }
 }
